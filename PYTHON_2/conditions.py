3641088dedd9be246ebfb8067a03431bcf17343b
Sean Donovan <donovans064@gmail.com>
2023-04-19 11:21:37 +0300

    Added conditions.py

---
+++ PYTHON_2/conditions.py
@@ -0,0 +1,8 @@
+n = input("Number: ")
+n = int(n) # converts n into an integer
+if n > 0:
+    print("Positive number")
+elif n < 0:
+    print("Negative number")
+else:
+    print("Number is zero")
