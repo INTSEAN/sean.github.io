5d0820a9817b1bd9404264bdc4427da2feeae5cb
Sean Donovan <donovans064@gmail.com>
2023-04-19 13:18:00 +0300

    Added decorator.py

--- PYTHON_2/decorator.py
+++ PYTHON_2/decorator.py
@@ -1,1 +1,12 @@
-print("Im a decorator!")
+def announce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with the function.")
+    return wrapper
+
+@announce
+def hello():
+    print("Hello world!")
+
+hello()
